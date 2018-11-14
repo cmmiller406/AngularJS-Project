2fb9c46460233c8d990b7db0dd23c4be300023fd
Hart <hwatnick@umich.edu>
2018-11-14 15:33:36 -0500

    a few more small changes

--- app/service/project-directive.js
+++ app/service/project-directive.js
@@ -4,18 +4,8 @@ function expandOnFocus () {
     return {
         restrict: "A",
         link: function($scope, $element, $attr) {
-            $element.on("blur", () => {
-                console.log("blur event");
-                $element.css("height", "");
-                $element.css("width", "");
-                $element.css("font-size", "");
-
-            });
-            $element.on("focus", () => {
-                console.log("focus event");
-                $element.css("height", "40px");
-                $element.css("width", "500px");
-                $element.css("font-size", "30px");
+            $element.on("click", () => {
+                $element.css("display",);
             });
         }
     };
